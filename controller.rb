cba49abd54c4446d50721e46c728121e6c025a98
Fabio Gelsomini <fgelsomini@gmail.com>
2015-04-19 18:48:23 -0400

    Initial commit

---
+++ controller.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require_relative 'models/movie'
+
+# Create route to show all movies
+
+# Create route to show form for a new movie
+
+# Create route to create a new movie when form is submitted
+
+# Create route to show one specific movie by ID
+
+# Create route to show form to edit one specific movie
+
+# Create route to update movie when edit form is submitted
+
+# Create route to delete a movie 
+
+
+
---
+++ models/movie.rb
@@ -0,0 +1,70 @@
+class Movie
+
+  attr_accessor :title, :director, :rating
+  attr_reader :id
+
+  @@all = []
+  @@counter = 1
+
+  def initialize(attributes={})
+    @title = attributes[:title]
+    @director = attributes[:director]
+    @rating = attributes[:rating]
+  end
+
+  def save
+    @id = @@counter
+    @@all << self
+    @@counter += 1
+  end
+
+  def update(attributes={})
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  def destroy
+    @@all.delete(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find(id)
+    movie = self.all.find {|movie| movie.id == id.to_i}
+  end
+
+  def self.create(attributes={})
+    movie = Movie.new(attributes)
+    movie.save
+  end
+
+end
+
+
+# USAGE
+
+# New movie using create (set and save automatically)
+# Movie.create({:title => "Raiders of the Lost Ark", :director => "Spielberg", :rating => 10})
+
+# New movie (set and save manually)
+# m2 = Movie.new
+# m2.title = "Raiders of the Lost Ark"
+# m2.director = "Spielberg"
+# m2.rating = 10
+# m2.save
+
+# Get all movies
+# movies = Movie.all
+
+# Find movie by ID
+# m1 = Movie.find(1)
+
+# Update a movie (any number of attributes)
+# m1.update(:director => "Steven Spielberg")
+
+# Destroy movie
+# m2.destroy
+
